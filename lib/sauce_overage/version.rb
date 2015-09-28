4df608e280a36d5dcc0774a8817daba445d2fcf8
bootstraponline <code@bootstraponline.com>
2015-09-28 09:54:00 -0400

    Release 1.0.0

--- lib/sauce_overage/version.rb
+++ lib/sauce_overage/version.rb
@@ -1,4 +1,4 @@
 module SauceOverage
-  VERSION = '0.0.2' unless defined? ::SauceOverage::VERSION
-  DATE    = '2015-09-21' unless defined? ::SauceOverage::DATE
+  VERSION = '1.0.0' unless defined? ::SauceOverage::VERSION
+  DATE    = '2015-09-28' unless defined? ::SauceOverage::DATE
 end
